64ef31d3bfca2ccce16bc92c1f52a45db98f755c
Trevor Seitz <trevinla@gmail.com>
2018-04-20 20:45:40 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,7 @@ class InteractiveRecord
     end
 
     def self.find_by(hash)
-      sql = "SELECT * FROM #{self.table_name} WHERE hash[key] = hash[value]"
+      sql = "SELECT * FROM #{self.table_name} WHERE #{hash[key]} = #{hash[value]}"
       DB[:conn].execute(sql)
     end
 end
